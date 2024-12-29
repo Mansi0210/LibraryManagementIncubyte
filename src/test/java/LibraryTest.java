50c93c91635a5eacae518865116245d2a6c473b1
Mansi0210 <mansitkr333@gmail.com>
2024-12-29 20:08:30 +0530

    Failure Test Case Added

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -13,6 +13,14 @@ public class LibraryTest {
         Book book = new Book("1234","Harry Potter","Mansi",2003);
         assertEquals(true, library.addBook(book));
     }
+    //Add Book Failure
+    @Test
+    public void testAddBookFailure(){
+        Book book1 = new Book("5678","Reach-Dad-Poor-Dad","Dhruv",2005);
+        Book book2 = new Book("5678","Reach-Dad-Poor-Dad","Dhruv",2005);
+        assertEquals(true,library.addBook(book1));
+        assertEquals(false,library.addBook(book2));
+    }
 
 
 }
